a7feb41bd9a39956a42bed73202a4f408d05622d
hanrah <gh.rahimy@gmail.com>
2025-01-15 21:20:13 +0330

    Make messages start from bottom

--- components/journal-logs/index.tsx
+++ components/journal-logs/index.tsx
@@ -5,9 +5,9 @@ import Props from "./types";
 const JournalLogs = (props: Readonly<Props>) => {
   const { messages } = props;
   return (
-    <div className="flex flex-col w-full space-y-2 ">
+    <div className="flex flex-col-reverse space-y-reverse  w-full space-y-2  h-full overflow-y-auto">
       {messages.map((message, index) => (
-        <div key={index} className={"flex justify-start "}>
+        <div key={index} className={"flex justify-start  items-end"}>
           <div
             className={`bg-blue-500 text-white p-3 rounded-lg shadow-md max-w-[80%] break-words w-auto`}
             style={{
