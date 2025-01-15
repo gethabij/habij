762c3e34acf4abc4f483461065ababa60b70114b
hanrah <gh.rahimy@gmail.com>
2025-01-15 23:06:55 +0330

    Show messages as bubbles

--- components/journal-logs/index.tsx
+++ components/journal-logs/index.tsx
@@ -9,7 +9,7 @@ const JournalLogs = (props: Readonly<Props>) => {
       {messages.map((message, index) => (
         <div key={index} className={"flex justify-start  items-end"}>
           <div
-            className={`bg-blue-500 text-white p-3 rounded-lg shadow-md max-w-[80%] break-words w-auto`}
+            className={`bg-blue-500 text-white p-3 shadow-md max-w-[80%] break-words w-auto  rounded-t-xl  rounded-br-xl`}
             style={{
               wordWrap: "break-word",
               whiteSpace: "pre-wrap", // Preserve line breaks in the message
